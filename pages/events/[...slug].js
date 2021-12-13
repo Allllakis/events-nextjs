f316cf0603f5d2bbdd424d45ebed4523c80d8815
Allllakis <allakiselova20@gmail.com>
2021-12-13 14:30:52 +0300

    working with filtered events page

--- pages/events/[...slug].js
+++ pages/events/[...slug].js
@@ -1,16 +1,70 @@
-import React from 'react'
+import { Fragment } from "react";
 import { useRouter } from "next/router";
+import { getFilteredEvents } from "../../dummy-data";
+import EventList from "../../components/events/event-list";
+import ResultsTitle from "../../components/events/results-title";
+import Button from "../../components/ui/button";
+import ErrorAlert from "../../components/ui/error-alert"
 
 const FilteredEventsPage = () => {
+  const router = useRouter();
+  const filterData = router.query.slug;
 
-    const router = useRouter()
-    const filterData = router.query.slug
-    console.log(router.query.slug)
+  if (!filterData) {
+    return <p className="center">Loading...</p>;
+  }
+
+  const filteredYear = filterData[0];
+  const filteredMonth = filterData[1];
+
+  const numYear = +filteredYear;
+  const numMonth = +filteredMonth;
+
+  if (
+    isNaN(numYear) ||
+    isNaN(numMonth) ||
+    numYear > 2025 ||
+    numYear < 2021 ||
+    numMonth > 12 ||
+    numMonth < 1
+  ) {
     return (
-        <div>
-            <h1>Filtered Events Page</h1>
+      <Fragment>
+        <ErrorAlert>
+          <p>Invalid Filter! Please adjust values!</p>
+        </ErrorAlert>
+        <div className="center">
+          <Button link="/events">Show All Events</Button>
         </div>
-    )
-}
+      </Fragment>
+    );
+  }
 
-export default FilteredEventsPage
+  const filteredEvents = getFilteredEvents({
+    year: numYear,
+    month: numMonth,
+  });
+
+  if (!filteredEvents || filteredEvents.length === 0) {
+    return (
+      <Fragment>
+          <ErrorAlert>
+          <p>No events found!</p>
+          </ErrorAlert>   
+        <div className="center">
+          <Button link="/events">Show All Events</Button>
+        </div>
+      </Fragment>
+    );
+  }
+
+  const date = new Date(numYear, numMonth - 1);
+  return (
+    <Fragment>
+      <ResultsTitle date={date} />
+      <EventList items={filteredEvents} />
+    </Fragment>
+  );
+};
+
+export default FilteredEventsPage;
