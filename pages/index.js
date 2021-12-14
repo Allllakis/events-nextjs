09943de9aad88beb84363734bc213281102f5c0b
Allllakis <allakiselova20@gmail.com>
2021-12-14 19:20:55 +0300

    using fetch for homepage

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,12 @@
 import React, { Fragment } from "react";
 import { useRouter } from "next/router";
-import { getFeaturedEvents } from "../dummy-data";
+import { getFeaturedEvents } from "../helper/api-util";
 import EventList from "../components/events/event-list";
 import EventSearch from "../components/events/events-search";
 
-const HomePage = () => {
+const HomePage = (props) => {
   const router = useRouter();
-  const featuredEvents = getFeaturedEvents();
+ 
 
   function findEventsHandler(year, month) {
     const fullPath = `/events/${year}/${month}`;
@@ -15,9 +15,18 @@ const HomePage = () => {
   return (
     <Fragment>
       <EventSearch onSearch={findEventsHandler} />
-      <EventList items={featuredEvents} />
+      <EventList items={props.events} />
     </Fragment>
   );
 };
 
+
+export async function getStaticProps() {
+  const featuredEvents = await getFeaturedEvents()
+  return {
+    props: {
+      events: featuredEvents
+    }
+  }
+}
 export default HomePage;
