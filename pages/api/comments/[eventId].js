c7858527968f43717d1abba5d9a6bd8a2914cf1b
Allllakis <allakiselova20@gmail.com>
2021-12-20 18:21:57 +0300

    connecting frontend to the comments api

--- pages/api/comments/[eventId].js
+++ pages/api/comments/[eventId].js
@@ -1,38 +1,44 @@
 function handler(req, res) {
   const eventId = req.query.eventId;
 
-  if (req.method === "POST") {
-    const { text, name, email } = req.body;
+  
+
+
+  if (req.method === 'POST') {
+    const { email, name, text } = req.body;
 
     if (
-      !email.includes("@") ||
-      !name.trim() === "" ||
+      !email.includes('@') ||
       !name ||
+      name.trim() === '' ||
       !text ||
-      !text.trim() === ""
+      text.trim() === ''
     ) {
-      res.status(422).json({ message: "Invalid input!!" });
+      res.status(422).json({ message: 'Invalid input.' });
+      
       return;
     }
 
-    console.log(text)
     const newComment = {
-        id: new.Date().toISOString(),
-        email,
-        name,
-        text
-    }
-    res.status(201).json({ message: "Get your comment!" , comment: newComment});
+      email,
+      name,
+      text,
+      eventId,
+    };
+    console.log(newComment)
+    res.status(201).json({message: "Signed up!"});
   }
 
-  if(req.method === 'GET') {
-      const dummyComments = [
-          {id: c1, name: 'Alla', text: 'First comments'},
-          {id: c2, name: 'Alex', text: 'Second comments'}
-      ];
-      res.status(201).json({ comments: dummyComments});
-      }
-  }
+ if(req.method === 'GET') {
+const dummyList = [
+  {id: 'c1', name: 'Alla' , text: 'First Comment' },
+  {id: 'c2', name: 'Alex' , text: 'Second Comment' }
+];
 
+res.status(200).json({comments: dummyList});
+ }
+
+  
+}
 
 export default handler;
