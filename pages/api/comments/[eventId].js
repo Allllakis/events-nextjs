fad28a5bac8ab9efe711e36422f668b779c21a16
Allllakis <allakiselova20@gmail.com>
2021-12-21 13:27:09 +0300

    getting comments from mongodb

--- pages/api/comments/[eventId].js
+++ pages/api/comments/[eventId].js
@@ -3,6 +3,10 @@ import { MongoClient } from "mongodb";
 async function handler(req, res) {
   const eventId = req.query.eventId;
 
+  const client = await MongoClient.connect(
+    "mongodb+srv://Allusy:C2OIt0YDA1XSOilw@cluster0.luxax.mongodb.net/comments?retryWrites=true&w=majority"
+  );
+
   if (req.method === "POST") {
     const { email, name, text } = req.body;
 
@@ -17,9 +21,7 @@ async function handler(req, res) {
 
       return;
     }
-    const client = await MongoClient.connect(
-      "mongodb+srv://Allusy:C2OIt0YDA1XSOilw@cluster0.luxax.mongodb.net/events?retryWrites=true&w=majority"
-    );
+    
     const newComment = {
       email,
       name,
@@ -29,19 +31,19 @@ async function handler(req, res) {
 
     const db = client.db()
 
-    const resalt = await db.collection("comments").insertOne(newComment);
-    console.log(resalt)
+   const result =  await db.collection('comments').insertOne(newComment);
+   
+   newComment.id =  result.insertedId
 
     res.status(201).json({ message: "Signed up!" });
   }
 
   if (req.method === "GET") {
-    const dummyList = [
-      { id: "c1", name: "Alla", text: "First Comment" },
-      { id: "c2", name: "Alex", text: "Second Comment" },
-    ];
+   
+    const db = client.db();
+    const documents = await db.collection('comments').find().sort({_id: -1}).toArray();
 
-    res.status(200).json({ comments: dummyList });
+    res.status(200).json({ comments: documents });
   }
   client.close()
 }
